ea172028761df02a4ffb8beab0bd1fed08828a11
yovellazerus <129679597+yovellazerus@users.noreply.github.com>
2025-03-29 02:56:41 +0300

    adding macros for testing and for loading labels

--- testMain.c
+++ testMain.c
@@ -8,6 +8,11 @@
 #include "CPU.h"
 #include "Memory.h"
 
+#define LABEL_LOW(LABEL) (LABEL % 0x0100)
+#define LABEL_HIGH(LABEL) (LABEL / 0x0100)
+
+#define LOAD_LABEL(LABEL, MEMORY) (MEMORY.label_table[LABEL] = #LABEL)
+
 void test_basic();
 
 int main()
@@ -44,6 +49,10 @@ void test_basic(){
     CPU cpu;
     word entry_point = 0x1000;
 
+    Memory_init(&memory, entry_point);
+    CPU_init(&cpu, NULL);
+    CPU_reset(&cpu, &memory);
+
     byte code1[] = {0xa9, 0xfe, 0x8d, 0x34, 0x12,
                     0xaa, 0x48, 0xa9, 0x06 ,0xa9, 0x48,
                     0x48, 0x48, 0x48 ,0x20, 0x00, 0x40, 
@@ -53,11 +62,18 @@ void test_basic(){
         0xa9, 0x00, 0xf0, 0x05, 0xa9, 0x01, 0x8d, 0x00, 0x20, 0x60,
     };
 
-    Memory_init(&memory, entry_point);
-    CPU_init(&cpu, NULL);
-    CPU_reset(&cpu, &memory);
-    Memory_load_code(&memory, "test_program", 0x1000, code1, ARRAY_SIZE(code1));
-    Memory_load_code(&memory, "function1", 0x4000, function1, ARRAY_SIZE(function1));
+    word res_AND = 0x2000;
+    LOAD_LABEL(res_AND, memory);
+
+    byte test_AND[] = {
+        0xa9, 0xab, 0x8d, LABEL_LOW(res_AND), LABEL_HIGH(res_AND),
+        0x29, 0x0f,
+    };
+
+    Memory_load_code(&memory, "test_AND", entry_point, test_AND, ARRAY_SIZE(test_AND));
+
+    // Memory_load_code(&memory, "test_program", entry_point, code1, ARRAY_SIZE(code1));
+    // Memory_load_code(&memory, "function1", 0x4000, function1, ARRAY_SIZE(function1));
 
     while (!cpu.hlt) CPU_execute(&cpu, &memory);
     
