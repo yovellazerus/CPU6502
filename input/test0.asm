1a1acbc93525244b7f9665622bb29ffb0f932867
yovellazerus <129679597+yovellazerus@users.noreply.github.com>
2025-08-27 00:24:19 +0300

    add more to test0, Strange bug: duplicates at the end of the file

--- input/test0.asm
+++ input/test0.asm
@@ -3,7 +3,17 @@ ARG0 = 0x00
 RET0 = 0x08
 
 main:
+    PHA
+    JSR foo
+    JSR _halt
+
+_halt:
+    JMP _halt
+
+foo:
+    LDA #0x42
+    STA WPTR
+    BEQ end
+    INC WPTRH
+end:
     RTS
-
-
-    
