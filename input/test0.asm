18b43c17e444e6fafac179b2f3fcb037e94f3874
yovellazerus <129679597+yovellazerus@users.noreply.github.com>
2025-09-19 00:44:42 +0300

    adding more to test0

--- input/test0.asm
+++ input/test0.asm
@@ -1,9 +1,32 @@
 
+ARG0 = $00
+ARG1 = $01
+
+TMP0 = $02
+
 .segment "CODE"
 RESET:
-    lda #$42
-    sta $ff
-    .byte $ff
+    lda #$11
+    sta ARG0
+    lda #$22
+    sta ARG1
+    jsr swap
+    .byte $ff   ; halt for debug
+
+; void swap(byte a, byte b)
+; modify: (none) 
+swap:
+    pha ; prologue
+
+    lda ARG0
+    sta TMP0 ; byte tmp = a
+    lda ARG1
+    sta ARG0 ; a = b
+    lda TMP0
+    sta ARG1 ; b = tmp
+
+    pla ; epilogue
+    rts
 
 nmi_handler:
     rti
--- makefile
+++ makefile
@@ -16,6 +16,8 @@ all: $(TARGET)
 $(TARGET): $(OBJ)
 	$(CC) $(CFLAGS) -o $(TARGET) $(OBJ)
 	$(RM) $(OBJ)
+	.\cc65-2.19\bin\ca65.exe .\input\test0.asm -o .\input\test0.o
+	.\cc65-2.19\bin\ld65.exe .\input\test0.o -C .\input\test0.cfg -o .\input\test0.bin
 
 %.o: ./src/%.c
 	$(CC) $(CFLAGS) -c $< -o $@
