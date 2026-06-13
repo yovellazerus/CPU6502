7a794c796c14e370817e8146bce84804e0123f41
yovellazerus <129679597+yovellazerus@users.noreply.github.com>
2026-06-13 18:05:30 +0300

    adding software stack setup to crt0.s

--- machin/crt0.s
+++ machin/crt0.s
@@ -1,8 +1,18 @@
+
 .segment "STARTUP"
 .global __STARTUP__
 .import _main
 
+.import __STACK_START__
+.importzp c_sp
+
 __STARTUP__:
+    
+    lda #<__STACK_START__
+    sta c_sp+0
+    lda #>__STACK_START__
+    sta c_sp+1
+
     jsr _main
     jmp *
 
