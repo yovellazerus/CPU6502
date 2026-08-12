d0e6e962a520bc169e1921d6bc750a3db8d56513
yovellazerus <129679597+yovellazerus@users.noreply.github.com>
2026-08-12 20:07:20 +0300

    processes count now 512

--- kernel/common.h
+++ kernel/common.h
@@ -29,7 +29,7 @@
 #define MAX_GLOBAL_OPEN_FILES 128
 #define MAX_REGISTER_DEVICES  8
 
-#define MAX_PROC_COUNT      256
+#define MAX_PROC_COUNT      512
 #define MAX_PROC_NAME       16
 #define MAX_FILES_PER_PROC  16
 
